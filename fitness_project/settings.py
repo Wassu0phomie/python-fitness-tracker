af8fe148ae53d8ba00c108218ccf7a85b0b78e82
Simakov Egor <simakov_zhorochka@mail.ru>
2026-03-17 17:38:17 +0300

    style

--- fitness_project/settings.py
+++ fitness_project/settings.py
@@ -143,3 +143,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'index'  # Имя пути из main/urls.py
 LOGOUT_REDIRECT_URL = 'login' # Куда идти после выхода
+
+LOGIN_URL = 'login'
